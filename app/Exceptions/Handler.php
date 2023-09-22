0bb9ca0d9683ee77947fa89bef4dc37408c2d7e0
Tejas Soni <tejas.tejas.soni3@gmail.com>
2023-09-22 12:06:19 +0530

    sanctum updates

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,11 +2,15 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use App\Traits\HttpResponses; // KEY : SANCTUM
+use Illuminate\Auth\AuthenticationException; 
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+
+    use HttpResponses; // KEY : SANCTUM
     /**
      * The list of the inputs that are never flashed to the session on validation exceptions.
      *
@@ -22,7 +26,15 @@ class Handler extends ExceptionHandler
      * Register the exception handling callbacks for the application.
      */
     public function register(): void
-    {
+    {   
+        // KEY : SANCTUM starts
+        $this->renderable(function (AuthenticationException $e, $request) {
+            if ($request->is('api/*')) {
+                return $this->error([],"Unauthenticated.",401);              
+            }
+        });
+        // KEY : SANCTUM ends
+
         $this->reportable(function (Throwable $e) {
             //
         });
